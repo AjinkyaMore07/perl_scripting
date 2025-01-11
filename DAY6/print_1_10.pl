3e072d332afc4095639b24a9c61a511774cb48a8
Ajinkya Bhausaheb More <113751745+AjinkyaMore07@users.noreply.github.com>
2025-01-11 21:34:31 +0530

    Create print_1_10.pl

---
+++ DAY6/print_1_10.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl -w
+use strict;
+my $i = 0;
+my $j = 10;
+do {
+        print "$i\n";
+        $i+=1;
+   } while ($i < 10);
+
+do {
+        print "$j \n";
+        $j-=1;
+  } until ($j <0)
+
