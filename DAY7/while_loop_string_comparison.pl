b0471f170e97860124572112aba3eb059507cd3c
Ajinkya Bhausaheb More <113751745+AjinkyaMore07@users.noreply.github.com>
2025-01-12 20:02:13 +0530

    Create while_loop_string_comparison.pl

---
+++ DAY7/while_loop_string_comparison.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl -w
+use strict ;
+
+while (1)
+ {	
+	 if($ARGV eq "INDIA")
+	 {
+		 print "India is also well known as the land of spirituality, philosophy, science, and technology \n";
+	 	last;
+ 	 }
+
+	
+ }
