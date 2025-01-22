c571b0391d58e51280b28f132fa2ba5cb9d86eb9
Ajinkya Bhausaheb More <113751745+AjinkyaMore07@users.noreply.github.com>
2025-01-22 07:35:49 +0530

    Create for_loop.pl

---
+++ DAY15/for_loop.pl
@@ -0,0 +1,9 @@
+#!/usr/bin/perl -w
+use strict;
+
+my @array = " Welcome to India the land of cultural heritage and great freedom fighters!";
+
+for ( $_ = 0 ; $_ <=($#array) ; $_++)
+ {
+	 print "$array[$_] ";
+ }
